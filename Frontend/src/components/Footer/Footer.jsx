da787359d1bbbfa6b00e8e2f22fecf4c24e7bb92
Ali Ebadi <aliebadienir@gmail.com>
2025-09-26 19:31:30 +0330

    Added info for footer

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { FaMapMarkerAlt, FaTwitter, FaWhatsapp, FaTelegramPlane } from 'react-icons/fa';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 import { IoCall, IoLogoLinkedin } from 'react-icons/io5';
-import { BsFillPrinterFill } from 'react-icons/bs';
-import { RiInstagramFill } from 'react-icons/ri';
 import './Footer.css';
 import logo from '../../assets/LOGO.png';
 
 function Footer() {
   // Static data to replace the dynamic fetching
   const siteInfo = {
-    address: '123 Main Street, Tehran, Iran',
-    phone_numbers: ['+98 21 1234 5678', '+98 21 8765 4321'],
-    instagram: 'https://www.instagram.com/example',
-    telegram: 'https://t.me/example',
-    whatsapp: '989123456789', // without the '+' prefix for WhatsApp link
+    address: 'امیر آباد شمالی، پارک علم و فناوری دانشگاه تهران',
+    phone_numbers: ['09228943180'],
+    linkedin: 'https://www.linkedin.com/company/oppany/posts/',
   };
 
   return (
@@ -28,32 +24,16 @@ function Footer() {
 
           {/* Phone Numbers Section */}
           <div className='footer-content-text-single'>
-            {siteInfo.phone_numbers.map((number, index) => (
-              <React.Fragment key={index}>
-                <BsFillPrinterFill className='footer-icon' />
-                <p>{number}</p>
-                <IoCall className='footer-icon' />
-                <p>{number}</p>
-              </React.Fragment>
-            ))}
+            <IoCall className='footer-icon' />
+            <p>تلفن: {siteInfo.phone_numbers[0]}</p>
           </div>
 
           {/* Social Media Section */}
           <div className='footer-content-text-single'>
             <p>شبکه های اجتماعی</p>
-            {siteInfo.instagram && (
-              <a href={siteInfo.instagram} target="_blank" rel="noopener noreferrer">
-                <RiInstagramFill className='footer-icon' />
-              </a>
-            )}
-            {siteInfo.telegram && (
-              <a href={siteInfo.telegram} target="_blank" rel="noopener noreferrer">
-                <FaTelegramPlane className='footer-icon' />
-              </a>
-            )}
-            {siteInfo.whatsapp && (
-              <a href={`https://wa.me/${siteInfo.whatsapp}`} target="_blank" rel="noopener noreferrer">
-                <FaWhatsapp className='footer-icon' />
+            {siteInfo.linkedin && (
+              <a href={siteInfo.linkedin} target="_blank" rel="noopener noreferrer">
+                <IoLogoLinkedin className='footer-icon' />
               </a>
             )}
           </div>
